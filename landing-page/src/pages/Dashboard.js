34dc4045a5c1b08734aafe8a96784499bfc38460
Caline <calineyammine123@gmail.com>
2022-03-01 01:05:47 +0200

    Designed dashboard

--- landing-page/src/pages/Dashboard.js
+++ landing-page/src/pages/Dashboard.js
@@ -1,21 +1,67 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import axios from 'axios';
 import NavigationDashboard from '../components/NavigationDashboard';
 
 
 const Dashboard = () => {
-    let redirect=useNavigate();
-    if (localStorage.getItem("id")===null){
-        redirect("/register");
-        return;
-    }
+    const [name, setName]= useState('');
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState('');
+
+    async function submit(e) {
+        e.preventDefault();
+        
+
+        const user= {
+          name,
+          email,
+          password,
+        };
+        //console.log(user);
+        const url = "http://127.0.0.1:8000/api/auth/register";
+        axios.post(url, user)
+        .then(function (response) {
+            if (response.data.message==="User successfully registered"){
+                
+            }
+         })
+         .catch(function (error) {
+             console.log('Invalid credentials');
+             document.getElementById("message").innerHTML = "<p style='color: red'>Invalid credentials!</p>";
+         });
+      }
     return (
         <div>
             <NavigationDashboard/>
-        Dashboard
-        <p>{localStorage.getItem("name")}</p>
-        <p>{localStorage.getItem("email")}</p>
+        <h1>Hello {localStorage.getItem("name")}!</h1>
+
+
+        <form className='container mt-5' onSubmit={submit}>
+            <h1 className="h3 mb-3 fw-normal">Edit your info below</h1>
+
+            <div className="form-floating">
+                <input className="form-control" placeholder="Full name" required
+                onChange={e=>setName(e.target.value)}/>
+                <label htmlFor="floatingInput">{localStorage.getItem("name")}</label>
+            </div>
+            <div className="form-floating">
+                <input type="email" className="form-control" placeholder="name@example.com" required
+                onChange={e=>setEmail(e.target.value)}/>
+            <label htmlFor="floatingInput">{localStorage.getItem("email")}</label>
+            </div>
+                <div className="form-floating">
+                <input type="password" className="form-control" placeholder="Password" required
+                onChange={e=>setPassword(e.target.value)}/>
+            <label htmlFor="floatingPassword">Password</label>
+            </div>
+
+            <div id='message'></div>
+            
+            <button className="w-100 btn btn-lg btn-success" type="submit">Save</button>
+        </form>
         </div>
+        
   )
 }
 

b905e3b2dee3ba822d46377c86ef57f189cf7da3
Caline <calineyammine123@gmail.com>
2022-02-28 19:06:49 +0200

    Added about us section

---
+++ landing-page/src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <h1>About us</h1>
+        <p>HireUp is the place to connect employers and freelancers.<br/>
+         The idea behind the application is to let you find the freelancer you need to get that little task or even that big project done.<br/>
+         You can add your services and get hired too!</p>
+    </div>
+  )
+}
+
+export default AboutUs
--- landing-page/src/pages/Home.js
+++ landing-page/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutUs from '../components/AboutUs'
 import CarouselComponent from '../components/CarouselComponent'
 
 
@@ -6,13 +7,16 @@ import CarouselComponent from '../components/CarouselComponent'
 const Home = () => {
   return (
     <div>
-    <h1 className='title'>Find someone that will get the job done.</h1>
-    <h2 className='title1'>HireUp is the app where you can hire freelancers and find freelance jobs.</h2>
-    <div style={{marginBottom:'25px'}}>
-    <h2>Check it out</h2>
-    <span className='icon'>&#8681;</span>
-    </div>
-    <CarouselComponent/>
+        <h1 className='title'>Find someone that will get the job done.</h1>
+        <h2 className='title1'>HireUp is the app where you can hire freelancers and find freelance jobs.</h2>
+        <div style={{marginBottom:'25px'}}>
+        <h2>Check it out</h2>
+        <span className='icon'>&#8681;</span>
+        </div>
+        <CarouselComponent/>
+        <div className='aboutus'>
+            <AboutUs/>
+        </div>
     </div>
   )
 }

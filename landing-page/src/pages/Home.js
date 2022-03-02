d874eebadfe557330e47efd4d05bb10609834260
Caline <calineyammine123@gmail.com>
2022-03-03 01:06:21 +0200

    Edited home page

--- landing-page/src/pages/Home.js
+++ landing-page/src/pages/Home.js
@@ -11,11 +11,12 @@ const Home = () => {
     <div className='containerFluid'>
         <Navigation/>
         <div className='homediv'>
-            <h1 className='title'>Find someone that will get the job done.</h1>
-            <h2 className='title1'>HireUp is the app where you can hire freelancers and find freelance jobs.</h2>
+            <h1 className='title1'>HireUp</h1> 
+            <h2 className='subtitle'>Hire freelancers and find freelance jobs.</h2>
+            <h1 className='title'>Find someone who will get the job done.</h1>
         </div>
         <div style={{marginBottom:'25px'}}>
-            <h2>Check it out</h2>
+            <h2>Find out more</h2>
             <span className='icon'>&#8681;</span>
         </div>
         <CarouselComponent/>
